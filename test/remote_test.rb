eb4b1f0ed4a9203996cff5f8ed0065eff6d60442
Eric Hu and Anthony Dreessen <hu.eric@gmail.com>
2015-01-31 13:59:33 -0800

    WIP: add pending test shells

--- test/remote_test.rb
+++ test/remote_test.rb
@@ -2,6 +2,12 @@ require 'mocha/mini_test'
 
 module Byebug
   module RemoteTest
+    class InterruptTest < TestCase
+      def test_interrupt_calls_interrupt_on_current_context
+        skip("not sure how to mock out a local provided by a C-extension")
+      end
+    end
+
     class StartServerTest < TestCase
       def teardown
         # Byebug#start_server defines an instance variable on the Byebug
@@ -26,6 +32,22 @@ module Byebug
         Byebug.start_server(&block)
 
         assert_equal(true, yielded)
+      end
+
+      def test_start_server_clears_handler_interface
+        skip("determine how handler (local variable?) is set to begin with")
+      end
+
+      def test_start_server_starts_byebug
+        skip("determine what Byebug.start does")
+      end
+
+      def test_start_server_starts_a_tcp_server_with_the_specified_host_and_port
+        skip("fails when run first: first call to start_control exits method early")
+        Mutex.stubs(:new)
+        mock_server = mock("server", addr: [0, 1])
+        TCPServer.expects(:new).with("some host", 1234).returns(mock_server)
+        Byebug.start_server("some host", 1234)
       end
     end
   end
